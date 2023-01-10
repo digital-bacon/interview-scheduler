490668ddb2d23a672fcf4d9c767bcc38f76c5d30
Mark Bieganek <mark.bieganek@telus.com>
2023-01-10 13:32:20 -0700

    Assign props within <InterviewerListItem>

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -6,10 +6,10 @@ export default function InterviewerListItem ({ id, name, avatar, selected: isSel
     <li className="interviewers__item">
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={ avatar }
+        alt={ name }
       />
-      Sylvia Palmer
+      { name }
     </li>
   );
 }
