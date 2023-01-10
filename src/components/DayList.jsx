6d8b32b1e07f6c8c99e827682de160b0ea3602ee
Mark Bieganek <mark.bieganek@telus.com>
2023-01-10 15:25:07 -0700

    Fix: Add missing selected property and logic to <DayListItem>

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import DayListItem from "./DayListItem.jsx";
 
-export default function DayList({ days: daysArray, setDay, ...props }) {
+export default function DayList({ days: daysArray, day: currentDayName, setDay, ...props }) {
   const days = daysArray.map(day => {
     return (
       <DayListItem
         key={ day.id }
         name={ day.name }
         spots={ day.spots }
+        selected={ day.name === currentDayName }
         setDay={ setDay }
       />
     )
