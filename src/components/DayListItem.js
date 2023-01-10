9a2245f41cba72997a0950533d8064373bf6d630
Mark Bieganek <mark.bieganek@telus.com>
2023-01-10 08:30:11 -0700

    Apply <DayListItem> prop values {name} {spots} and handle click

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,9 +11,9 @@ export default function DayListItem(
 ) {
 
   return (
-    <li>
-      <h2 className='text--regular'>Day Name</h2>
-      <h3 className='text--light'>X spots remaining</h3>
+    <li onClick={() => setDay(name)}>
+      <h2 className='text--regular'>{name}</h2>
+      <h3 className='text--light'>{spots} spots remaining</h3>
     </li>
    );
 }
