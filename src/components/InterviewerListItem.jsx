4c97ab608105a40d342dcbdc981bd664fb486b0b
Mark Bieganek <mark.bieganek@telus.com>
2023-01-10 13:38:34 -0700

    Handle onClick <InterviewerListItem>

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
 
-export default function InterviewerListItem ({ id, name, avatar, selected: isSelected, ...props } = props) {
+export default function InterviewerListItem ({ id, name, avatar, selected: isSelected, setInterviewer, ...props } = props) {
   return (
-    <li className="interviewers__item">
+    <li className="interviewers__item" onClick={ () => setInterviewer(id) }>
       <img
         className="interviewers__item-image"
         src={ avatar }
