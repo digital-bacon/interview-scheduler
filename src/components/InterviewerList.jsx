a2925a6a13b97db5b02d73e27a375577a69e6d48
Mark Bieganek <mark.bieganek@telus.com>
2023-01-10 15:24:23 -0700

    Build interviewers array with <InterviewerListItem> components>

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerList ({ interviewers, ...props }) {
+export default function InterviewerList ({
+  interviewers: interviewersArray,
+  interviewer: currentInterviewerId,
+  setInterviewer,
+  ...props }) {
+
+  const interviewers = interviewersArray.map(interviewer => {
+    return (
+        <InterviewerListItem
+          key={ interviewer.id }
+          id={ interviewer.id }
+          name={ interviewer.name }
+          avatar={ interviewer.avatar }
+          selected={ currentInterviewerId === interviewer.id ? true : false }
+          setInterviewer={ setInterviewer }
+        />
+      );
+  })
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        { interviewers }
+      </ul>
     </section>
   );
 }
