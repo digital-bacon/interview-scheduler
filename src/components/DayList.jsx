29b43ee3cd97816808c5daea6bcdc5aa4c7459a0
Mark Bieganek <mark.bieganek@telus.com>
2023-01-10 17:03:56 -0700

    Refactor <DayList> prop names 'day, setDay' to 'value, onChange'

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import DayListItem from "./DayListItem.jsx";
 
-export default function DayList({ days: daysArray, day: currentDayName, setDay, ...props }) {
+export default function DayList({ days: daysArray, value, onChange, ...props }) {
   const days = daysArray.map(day => {
     return (
       <DayListItem
         key={ day.id }
         name={ day.name }
         spots={ day.spots }
-        selected={ day.name === currentDayName }
-        setDay={ event => setDay(day.name) }
+        selected={ day.name === value }
+        setDay={ onChange }
       />
     )
   })
