6ee98f2ec5bae8a7521635c9f7c47dfeaea7d2b0
Mark Bieganek <mark.bieganek@telus.com>
2023-01-13 16:24:00 -0700

    Improve function names for self-documentation

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,28 +3,19 @@ import { useState } from "react";
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
+  
+  const copyOfHistoryWithoutLastIndex = () => [...history.slice(0, -1)];
+  
+  const copyOfHistory = () => [...history];
 
-  const getArrayWithoutLastElement = (array) => {
-    if (array.length > 0) {
-      return [ ...array.slice(0, -1) ];
-    }
-  
-    return [ ...history ];
-  }
-  
-  const getArrayLastValue = (array) => {
-    if (array.length > 0) {
-      return array[array.length -1];
-    }
-  }
-  
   const addToHistory = (newMode, replaceLast = false) => {
-    const buffer = replaceLast ? getArrayWithoutLastElement(history) : [ ...history ];
-    if (newMode !== getArrayLastValue(buffer)) {
-      return setHistory([ ...buffer, newMode ]);
+    const buffer = replaceLast ? copyOfHistoryWithoutLastIndex() : copyOfHistory();
+    const bufferLastValue = [...buffer].pop();
+    if (newMode !== bufferLastValue) {
+      return setHistory([...buffer, newMode]);
     }
 
-    return setHistory([ ...buffer ]);
+    return setHistory([...buffer]);
   }
   
   const transition = (newMode, replaceLast = false) => {
@@ -34,7 +25,7 @@ const useVisualMode = (initial) => {
   
   // Function that transitions to the previous mode in the history state
   const back = () => {
-    const buffer = getArrayWithoutLastElement(history);
+    const buffer = copyOfHistoryWithoutLastIndex();
     const newMode = buffer.length > 0 ? buffer.pop() : initial;
     transition(newMode, true);
   };
