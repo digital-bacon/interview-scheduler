c5f1f8f2bbfe19839657212e88489514c79e8165
Mark Bieganek <mark.bieganek@telus.com>
2023-01-12 17:27:59 -0700

    Create a back method and unit test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,31 @@ import React, { useState } from "react";
 
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
-  
+  const [history, setHistory] = useState([initial]);
+  // console.log('history: ', history);
+
+  const transition = newMode => {
+    setHistory(() => [ ...history, newMode ]);
+    setMode(() => newMode);
+  };
+
+  const back = () => {
+    if (history.length > 0) {
+      const removed = history.pop();
+      setHistory(() => [ ...history ])
+    }
+    
+    let newMode = initial;
+    if (history.length > 0) {
+      newMode = history[history.length - 1]
+    }
+    setMode(() => newMode);
+  };
+
   return { 
     mode,
-    transition: newMode => setMode(newMode)
+    transition,
+    back,
    };
 };
 
