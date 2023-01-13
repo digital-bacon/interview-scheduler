2854b0b2c98305bd6a3b969f8b00763e5b86427f
Mark Bieganek <mark.bieganek@telus.com>
2023-01-13 16:01:41 -0700

    Refactor to only mutate history array with setHistory()

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,34 +3,42 @@ import { useState } from "react";
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  // console.log('history: ', history);
 
-  const removeLastFromHistory = () => {
-    if (history.length > 0) {
-      return history.pop();
+  const getArrayWithoutLastElement = (array) => {
+    if (array.length > 0) {
+      return [ ...array.slice(0, -1) ];
+    }
+  
+    return [ ...history ];
+  }
+  
+  const getArrayLastValue = (array) => {
+    if (array.length > 0) {
+      return array[array.length -1];
     }
   }
   
-  const transition = (newMode, replace = false) => {
-    // When replace is true then set the history to reflect that we are replacing the current mode.
-    if (replace) {
-      removeLastFromHistory();
+  const addToHistory = (newMode, replaceLast = false) => {
+    const buffer = replaceLast ? getArrayWithoutLastElement(history) : [ ...history ];
+    if (newMode !== getArrayLastValue(buffer)) {
+      return setHistory([ ...buffer, newMode ]);
     }
 
-    setHistory(() => [ ...history, newMode ]);
-    setMode(() => newMode);
+    return setHistory([ ...buffer ]);
+  }
+  
+  const transition = (newMode, replaceLast = false) => {
+    addToHistory(newMode, replaceLast);
+    setMode(newMode);
   };
-
+  
+  // Function that transitions to the previous mode in the history state
   const back = () => {
-    removeLastFromHistory();
-    let newMode = initial;
-    if (history.length > 0) {
-      newMode = history[history.length - 1]
-    }
-
-    setMode(() => newMode);
+    const buffer = getArrayWithoutLastElement(history);
+    const newMode = buffer.length > 0 ? buffer.pop() : initial;
+    transition(newMode, true);
   };
-
+  
   return { 
     mode,
     transition,
