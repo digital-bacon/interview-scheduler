0c91690e66b43424cbad39e4387e97669ec54a76
Mark Bieganek <mark.bieganek@telus.com>
2023-01-14 20:09:46 -0700

    Refactor to improve pure functions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,34 +4,31 @@ const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const copyHistory = (callback) => {
-    const newArray = [...history];
-    return callback ? callback(newArray) : newArray;
-  }
-
-  const addToHistory = (newMode, replaceLastMode = false) => {
-    const newHistory = replaceLastMode ? copyHistory(dropLastIndex) : copyHistory();
-    const lastModeInHistory = getLastElement(newHistory);
-    if (newMode !== lastModeInHistory) {
-      return setHistory([...newHistory, newMode]);
-    }
-
-    return setHistory([...newHistory]);
-  }
-  
   const transition = (newMode, replaceLastMode = false) => {
     addToHistory(newMode, replaceLastMode);
     setMode(newMode);
   };
-  
+
   const back = () => {
     const newMode = getPreviousMode();
     transition(newMode, true);
   };
 
-  const dropLastIndex = array => array.slice(0, -1);
+  const addToHistory = (newMode, replaceLastMode = false, newBaseHistoryArray) => {
+    if (replaceLastMode) {
+      return replaceLastHistory(newMode);
+    }
+    
+    const historyBuffer = !newBaseHistoryArray ? copyHistory() : [...newBaseHistoryArray];
+    const isUniqueNewMode = isUniqueNewElement(newMode, historyBuffer);
+    const newHistory =  isUniqueNewMode ? [...historyBuffer, newMode] : [...historyBuffer];
+    setHistory(newHistory);
+  };
 
-  const getLastElement = array => array[array.length - 1];
+  const replaceLastHistory = (newMode) => {
+    const historyBuffer = copyHistory(dropLastIndex);
+    addToHistory(newMode, false, historyBuffer);
+  }
 
   const getPreviousMode = () => {
     if (history.length >= 2) {
@@ -40,6 +37,12 @@ const useVisualMode = (initial) => {
     
     return history[history.length - 1];
   }
+
+  const copyHistory = (callback) => callback ? callback([...history]) : [...history];
+
+  const isUniqueNewElement = (element, array) => element !== array[array.length - 1];
+
+  const dropLastIndex = array => array.slice(0, -1);
 
   return {
     mode,
