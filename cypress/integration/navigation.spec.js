2ec5ae46589d9f1e8ae7b46ee47429a479292968
Mark Bieganek <mark.bieganek@telus.com>
2023-01-18 13:08:33 -0700

    Use reset the database and visit the app before each test in navigation.spec.js

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,14 +1,13 @@
 describe('Navigation', () => {
-  it('should visit root', () => {
+  
+  beforeEach(() => {
+    cy.visit('api/debug/reset');
     cy.visit('/');
   });
 
   it('should navigate to Tuesday', () => {
-    cy.visit('/');
-
     cy.contains('[data-testid=day]', 'Tuesday')
       .click()
       .should('have.class', 'day-list__item--selected');
-
   });
 });
