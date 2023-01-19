46ecbb93260066bafb240f8d5a4d788d0e4bcbe4
Mark Bieganek <mark.bieganek@telus.com>
2023-01-19 14:22:07 -0700

    Add function documentation, use previous state when setting state

--- src/hooks/useFormData.js
+++ src/hooks/useFormData.js
@@ -1,9 +1,27 @@
 import { useState } from "react";
 
-const useFormData = (initialValues, validationErrors, onSave, onCancel) => {
-  const [formData, setFormData] = useState(initialValues);
-  const [error, setError] = useState("");
+/**
+ * React hook to retrieve data from form and manage form state
+ * @param {Object} initialFormData - from app state
+ * @param {Object} validationErrors - pre-configured error messages to use in the error state
+ * @param {Function} onSave - callback that receives data from formData state
+ * @param {Function} onCancel - callback to be called when cancel() is executed
+ * @returns {Object} with properties:
+ *  formData (Object)
+ *  handleSubmit (Function)
+ *  cancel (Function)
+ *  validate (Function)
+ *  onChange (Function)
+ *  error (Object)
+ */
+const useFormData = (initialFormData, validationErrors, onSave, onCancel) => {
+  const initialError = "";
+  const [formData, setFormData] = useState(initialFormData);
+  const [error, setError] = useState(initialError);
 
+  /**
+   * Validates the form data and conditionally sets error or formData state
+   */
   const validate = () => {
     if (formData.name === "") {
       setError(validationErrors.empty.name);
@@ -18,21 +36,34 @@ const useFormData = (initialValues, validationErrors, onSave, onCancel) => {
     onSave(formData.name, formData.interviewer);
   };
 
+  /**
+   * When a form is submitted, prevent default behaviour and validate the inputs
+   * @param {Object} event - expects an event object
+   */
   const handleSubmit = (event) => {
     if (event) {
       event.preventDefault();
     }
-    
+
     validate();
   };
 
+  /**
+   * Handles a change on a form element
+   * @param {Object} event - expects an event object
+   */
   const onChange = (event) => {
     resetError();
     const target = event.target;
     const newData = getDataFrom(target);
-    setFormData({ ...formData, [newData.name]: newData.value });
+    setFormData(prev => prev = { ...prev, [newData.name]: newData.value });
   };
 
+  /**
+   * Gets data from a targetted element
+   * @param {Object} target - can be any target from an event
+   * @returns {Object} with the data retrieved as properties name and value
+   */
   const getDataFrom = (target) => {
     const initial = {
       name: "",
@@ -59,6 +90,11 @@ const useFormData = (initialValues, validationErrors, onSave, onCancel) => {
     return initial;
   };
 
+  /**
+   * Gets data from an input element
+   * @param {Object} input - can be any input element with attributes name and value
+   * @returns {Object} with the data retrieved as properties name and value
+   */
   const getDataFromInput = (input) => {
     const data = {
       name: "",
@@ -71,6 +107,11 @@ const useFormData = (initialValues, validationErrors, onSave, onCancel) => {
     return data;
   };
 
+  /**
+   * Gets data from an html element with data attributes
+   * @param {Object} input - can be any element with attributes data-name and data-value
+   * @returns {Object} with the data retrieved as properties name and value
+   */
   const getDataFromAttributes = (element) => {
     const data = {
       name: "",
@@ -90,17 +131,31 @@ const useFormData = (initialValues, validationErrors, onSave, onCancel) => {
     return data;
   };
 
+  /**
+   * Invoked when a user presses the cancel button on the form
+   */
   const cancel = () => {
     resetForm();
     onCancel();
   };
 
+  /**
+   * Calls the resetError callback to clear error state, and sets the formData state back to initial values
+   */
   const resetForm = () => {
     resetError();
-    setFormData({ ...initialValues });
+    resetFormData();
   };
 
-  const resetError = () => setError("");
+  /**
+   * Resets formData state to initial state
+   */
+  const resetFormData = () => setFormData(prev => prev = { ...initialFormData });
+
+  /**
+   * Resets error state to initial state
+   */
+  const resetError = () => setError(prev => prev = initialError);
 
   return { formData, handleSubmit, cancel, validate, onChange, error };
 };
